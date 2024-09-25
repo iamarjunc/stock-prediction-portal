95da72913d2b5c0b4edfe4287385de69ebdc054e
Arjun C <arjunchandrakumar2017@gmail.com>
2024-09-25 11:52:52 +0530

    COMMIT 25 sept-Home page react

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
-import './App.css'
+import './assets/css/style.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      <Header/>
+      <Main/>
+      <Footer/>
     </>
   )
 }
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from './Button'
+const Main = () => {
+  return (
+    <>
+      <div className='container'>
+        <div className="p-5 text-center bg-light-dark rounded">
+          <h1 className='text-light'>Stock Prediction Portal</h1>
+          <p className='text-light lead'>
+            Our platform leverages the power of Django and React to provide cutting-edge stock market predictions. Whether you're a seasoned investor or just getting started, our user-friendly interface and advanced algorithms will help you make informed decisions in the fast-paced world of stock trading. Stay ahead of the market with real-time data, accurate predictions, and personalized insights designed to help you maximize your investments.
+          </p>
+          <Button text ='Login' class="btn-outline-info"/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Main
