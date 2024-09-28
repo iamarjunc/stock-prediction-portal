adfa299ed3bacaec099fb85cf0545265e862a692
Arjun C <arjunchandrakumar2017@gmail.com>
2024-09-28 17:29:44 +0530

    Auth Functionalities

--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import axios from 'axios'
+import axiosInstance from '../../AxiosInstance'
 
 const Dashboard = () => {
+
+  useEffect(() => {
+    const fetchProtectedData = async () => {
+      try{
+        const response = await axiosInstance.get('/protected-view/');
+        console.log("Success", response.data);
+      }
+      catch(error){
+        console.error("Error fetching Data: ",error);
+      }
+    }
+    fetchProtectedData();
+  },[])
+
   return (
     <div>dashboard</div>
   )
