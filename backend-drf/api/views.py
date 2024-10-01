6dbea71bf01d7768ccc45380081f89b28c563c67
Arjun C <arjun@daveanit.com>
2024-10-01 18:07:12 +0530

    1 OCT Evening

--- backend-drf/api/views.py
+++ backend-drf/api/views.py
@@ -1,3 +1,40 @@
+from datetime import datetime
+import os
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import StockPredictionSerializer
+from rest_framework.response import Response
+from rest_framework import status
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Create your views here.
+
+class StockPredictionAPIView(APIView):
+    def post(self, request):
+        serializer = StockPredictionSerializer(data = request.data)
+        if serializer.is_valid():
+            ticker = serializer.validated_data['ticker']
+            # Fetch the data from yfinance
+            now = datetime.now()
+            start = datetime(now.year-10, now.month,now.day)
+            end = now
+            df = yf.download(ticker, start, end)
+            if df.empty:
+                return Response({"error":"No data found for the given ticker", "status":status.HTTP_404_NOT_FOUND})
+            df = df.reset_index()
+            # Generate Basic Plot
+            plt.switch_backend('AGG')
+            plt.figure(figsize=(12,5))
+            plt.plot(df.Close, label = 'Closing Price')
+            plt.title(f"Closing price of {ticker}")
+            plt.xlabel('Days')
+            plt.ylabel('Close price')
+            plt.legend()
+            # Save the plot to a file
+            plot_img_path = f"{ticker}_plot.png"
+            image_path = os
+            
+            
+            return Response({'status': 'success','ticker' : ticker})
--- frontend-react/src/components/dashboard/Dashboard.jsx
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -1,14 +1,17 @@
-import React,{useEffect} from 'react'
-import axios from 'axios'
+import React,{useEffect, useState} from 'react'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import axiosInstance from '../../AxiosInstance'
 
 const Dashboard = () => {
+  const [ticker, setTicker] = useState('')
+  const [error, setError] = useState('')
+  const [loadng, setLoading] = useState(false)
 
   useEffect(() => {
     const fetchProtectedData = async () => {
       try{
         const response = await axiosInstance.get('/protected-view/');
-        console.log("Success", response.data);
       }
       catch(error){
         console.error("Error fetching Data: ",error);
@@ -17,8 +20,40 @@ const Dashboard = () => {
     fetchProtectedData();
   },[])
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true)
+    try{
+      const response = await axiosInstance.post('/predict/',{
+        ticker: ticker
+      });
+      console.log(response.data);
+      if (response.data.error){
+        setError(response.data.error)
+      }
+    }
+    catch{
+      console.error("There was an error making the API request");
+    }
+    finally{
+    setLoading(false)
+    }
+  }
+
   return (
-    <div>dashboard</div>
+    <div className='container'>
+      <div className="row">
+        <div className="col-md-6 mx-auto">
+          <form onSubmit={handleSubmit}>
+            <input type="text" className='form-control' placeholder='Enter Stock Ticker' onChange={(e) => setTicker(e.target.value)} required/>
+            <small>{error && <div className="text-danger">{error}</div> }</small>
+            <button type="submit" className='btn btn-info mt-3'>
+              {loadng ? <span><FontAwesomeIcon icon={faSpinner} spin/>Please Wait...</span> : 'See Prediction' }
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
